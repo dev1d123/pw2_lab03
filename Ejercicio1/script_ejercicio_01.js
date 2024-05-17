6041b8e404d26ef8f4f1ed66f38ef4d43aa34700
dev1d123 <dhuamanio@unsa.edu.pe>
2024-05-17 18:14:33 -0500

    Genearcion de arreglo de enteros para ubicarlos en los botones

--- Ejercicio1/script_ejercicio_01.js
+++ Ejercicio1/script_ejercicio_01.js
@@ -1,1 +1,21 @@
 console.log("Exito")
+var elem = document.getElementsByTagName("body");
+console.log(elem);
+var random =Math.floor(Math.random()*10);
+console.log("Numero aleatorio!", random);
+
+var numerosAleatorios = new Set();
+
+while(numerosAleatorios.size != 10){
+    random =Math.floor(Math.random()*10);
+    console.log("Numero aleatorio!", random);
+    numerosAleatorios.add(random);
+}
+console.log(numerosAleatorios);
+var arreglo = [];
+
+numerosAleatorios.forEach(elem =>{
+    arreglo.push(elem);
+});
+
+console.log(arreglo);
