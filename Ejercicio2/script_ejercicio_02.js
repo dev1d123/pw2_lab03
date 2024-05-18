12610e967d729fbabb22a8b066d05bd440b37a98
dev1d123 <dhuamanio@unsa.edu.pe>
2024-05-17 19:06:00 -0500

    Se implemento una imitacion de una cola :(

--- Ejercicio2/script_ejercicio_02.js
+++ Ejercicio2/script_ejercicio_02.js
@@ -1,1 +1,21 @@
+
 console.log("Conexion exitosa");
+var input = document.getElementById("in");
+
+var basic = document.querySelectorAll(".b");
+basic.forEach(b =>{
+    b.addEventListener('click', function(){
+        console.log("click", b.textContent);
+        input = document.getElementById("in");
+        input.value = input.value + b.textContent;
+    });
+});
+
+var calcular = document.getElementById("calc");
+var stack = document.getElementById("stack");
+
+calcular.addEventListener('click', function(){
+    var result = eval(input.value);
+    input.value = result;
+    stack.innerHTML += result + "<br>";
+});
