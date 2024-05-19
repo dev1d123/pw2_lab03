81de50f3068a713c9f712dae2f869ca123fb9629
dev1d123 <139636102+dev1d123@users.noreply.github.com>
2024-05-18 22:19:26 -0500

    Version Final
    Se agrega una tabla para almacenar los elementos, se añaden mas estilos y se implementan los signos raros como sqrt o pow.

--- Ejercicio2/script_ejercicio_02.js
+++ Ejercicio2/script_ejercicio_02.js
@@ -1,21 +1,42 @@
-
 console.log("Conexion exitosa");
 var input = document.getElementById("in");
 
 var basic = document.querySelectorAll(".b");
-basic.forEach(b =>{
-    b.addEventListener('click', function(){
+basic.forEach(b => {
+    b.addEventListener('click', function() {
         console.log("click", b.textContent);
         input = document.getElementById("in");
-        input.value = input.value + b.textContent;
+        if (b.textContent === "Del") {
+            input.value = input.value.slice(0, -1);
+        } else if (b.textContent === "pi") {
+            input.value += Math.PI;
+        } else if (b.textContent === "sqrt") {
+            input.value += "Math.sqrt(";
+        } else if (b.textContent === "pow2") {
+            input.value += "**2";
+        } else {
+            input.value += b.textContent;
+        }
     });
 });
 
 var calcular = document.getElementById("calc");
-var stack = document.getElementById("stack");
+var stackTableBody = document.querySelector("#stack-table tbody");
 
-calcular.addEventListener('click', function(){
-    var result = eval(input.value);
-    input.value = result;
-    stack.innerHTML += result + "<br>";
+calcular.addEventListener('click', function() {
+    try {
+        var expression = input.value;
+        var result = eval(expression);
+        input.value = result;
+        
+        var newRow = stackTableBody.insertRow(0); 
+        var cellExpression = newRow.insertCell(0);
+        var cellResult = newRow.insertCell(1);
+        cellExpression.textContent = expression;
+        cellResult.textContent = result;
+        input.value = "";
+    } catch (error) {
+        alert("Error, expresion incorrecta");
+        input.value = "";
+    }
 });
